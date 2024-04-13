818ba882e42f5872b36146f517e7f1dc641b2dea
Guilhermesantol <guilhermesantossocialtrb@gmail.com>
2024-04-13 15:42:18 -0300

    criei a classe livro

---
+++ livro.java
@@ -0,0 +1,22 @@
+public class livro{
+    private String isbn;
+    private String titulo;
+
+
+    public String getisbn(){
+    return isbn;
+    }
+    public void setisbn(String isbn){
+        this.isbn = isbn;
+    }
+
+    public String gettitulo(){
+        return titulo;
+    }
+    public void settitulo(String titulo){
+        this.titulo = titulo;
+    }
+
+
+
+}
