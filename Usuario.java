9ebd45e078253fa9fd27092ea612c1116bfb00f1
Guilhermesantol <guilhermesantossocialtrb@gmail.com>
2024-04-13 16:10:32 -0300

    Create Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,17 @@
+public class Usuario {
+    private String login;
+    private String senha;
+
+    public String getlogin(){
+        return login;
+    }
+    public void setlogin(){
+        this.login = login;
+    }
+    public String getsenha(){
+        return senha;
+    }
+    public void setsenha(){
+        this.senha = senha;
+    }
+}
